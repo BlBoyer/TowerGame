42cbf5049cb77958476b415507e7dea34e6f73a4
BenB <boyer.benlee@gmail.com>
2022-06-18 10:19:59 -0700

    applied movement fix

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,11 +1,13 @@
 using UnityEngine;
+//mapbox unity sdk for 3D worlds
 
 public class PlayerMovement : MonoBehaviour
 {
     // Start is called before the first frame update
     public Transform PlayerT;
-    [Range(0,.5f)]
-    public float moveSpeed;
+    [Range(0, 5)]
+    public int moveSpeed;
+    private float speedVar;
     private float hInput;
     private float vInput;
     void Start()
@@ -21,12 +23,19 @@ public class PlayerMovement : MonoBehaviour
     }
     private void ProcessInputs() 
     {
-        hInput = Input.GetAxisRaw("Horizontal");
-        vInput = Input.GetAxisRaw("Vertical");
+        hInput = Input.GetAxisRaw("Horizontal") * .01f;
+        vInput = Input.GetAxisRaw("Vertical") * .01f;
     }
     private void Move()
     {
+        if (hInput > 0 && vInput > 0)
+        {
+            speedVar = moveSpeed * .7f;
+        } else 
+        {
+            speedVar = moveSpeed;
+        }
         var currentPos = new Vector3(PlayerT.position.x, PlayerT.position.y, PlayerT.position.z);
-        PlayerT.position = currentPos + new Vector3(hInput, vInput, 0) * moveSpeed;
+        PlayerT.position = currentPos + new Vector3(hInput, vInput, 0) * speedVar;
     }
 }
