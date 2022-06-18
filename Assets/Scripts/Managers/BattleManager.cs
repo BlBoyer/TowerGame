a748fa1a2617695542f4cc304df442e866517082
BenB <boyer.benlee@gmail.com>
2022-06-18 12:04:18 -0700

    started fleshing out battlesystem

--- Assets/Scripts/Managers/BattleManager.cs
+++ Assets/Scripts/Managers/BattleManager.cs
@@ -17,17 +17,53 @@ public class BattleManager : MonoBehaviour
             return _instance;
         }
     }
-    private string _nextScene;
-    void Start()
+    private string nextScene;
+    private IEnumerator battleState;
+    private bool running;
+    private bool atIdle;
+    private bool atAttack;
+    public GameObject playerPrefab; //we put a prefab here so we can instantiate it
+    public GameObject enemyPrefab; //this will be a NonSerialized public variable later, or just make a private method
+    public Transform playerStart;
+    public Transform enemyStart;
+    //flesh out the IEnumerator in start with initialized values, such as enemy, etc
+    void Awake()
     {
+        _instance = this;
+    }
+        //get enemyPrefab by enemy thac calls battle, we can change this variable from that script directly
+    private void SartBattle()
+    {
+        //get vals that we use in our coroutine
         //get player object
+        Instantiate(playerPrefab, playerStart);
+        Instantiate(enemyPrefab, enemyStart);
         //get enemy object
+        battleState = new IEnumerator()
+        {
+            //use values from args
+            //movePlayersTowardsOppositeSides()
+        if (running)
+        {
+            //move towards opposite sides
+            yield return null; //resume next frame, or wait for frames over time;
+        }
+        else if (atIdle)
+        {
+            //play idle animations for parties
+            yield return WaitForSeconds(3);
+        }
+        else if (atAttack)
+        {
+            yield return WaitForSeconds(attackDur);
+        }
     }
-    private void FixedUpdate()
+    }
+    void FixedUpdate()
     {
-        //move characters
-        //play animations
-    }
+    //move characters
+    //play animations
+    //if at players at start positions, running=false, idle for a couple seconds, then set running true coroutine
     // Update is called once per frame
     void Update()
     {
@@ -46,7 +82,8 @@ public class BattleManager : MonoBehaviour
     }
     private void End() 
     {
-        GameManager.instance.setScene(_nextScene);
+    //the reason we have a sep method for setting the scene is to be able to check the scene we're in, once we have set it, we can access it
+        GameManager.instance.setScene(nextScene);
         GameManager.instance.changeScene();
     }
 }
