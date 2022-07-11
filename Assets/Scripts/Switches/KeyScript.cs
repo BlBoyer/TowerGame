45c9339a21c83432054d7ed88c4f8b8e91752217
BenB <boyer.benlee@gmail.com>
2022-07-11 12:29:41 -0700

    updated some prefabs

--- Assets/Scripts/Switches/KeyScript.cs
+++ Assets/Scripts/Switches/KeyScript.cs
@@ -7,8 +7,7 @@ public class KeyScript : MonoBehaviour
     {
         if (other.gameObject.CompareTag("Player")) 
         {
-
-            //we may change this from gameObject to gameObject.name
+            //process getting key, notification and logic
             masterKey.GetComponent<KeyBuilder>().addKey(gameObject);
             //GetComponent<Collider2D>().enabled = false;
             gameObject.SetActive(false);
