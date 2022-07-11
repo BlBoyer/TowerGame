0313112ce49baf59505f43ece9dcc00d5c4c8d06
BenB <boyer.benlee@gmail.com>
2022-07-10 18:44:56 -0700

    tried using a vector, but it seems the same, the side-movement looks better with an else if

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -6,18 +6,17 @@ public class PlayerMovement : MonoBehaviour
 {
     // Start is called before the first frame update
     public Transform PlayerT;
-    private Rigidbody2D _rb;
     public Animator anim;
     [Range(0, 9)]
     public int moveSpeed;
     private float speedVar;
     private float hInput;
     private float vInput;
+    private Vector3 moveVector;
     private string walk_dir = "player_idle";
     private int _playerHealth;
     void Start()
     {
-        _rb = GetComponent<Rigidbody2D>();
         _playerHealth = this.GetComponent<Fighter>().health;
 
     }
@@ -42,6 +41,7 @@ public class PlayerMovement : MonoBehaviour
     {
         hInput = Input.GetAxisRaw("Horizontal"); //* .1f;
         vInput = Input.GetAxisRaw("Vertical"); //* .1f;
+        moveVector = new Vector3(hInput, vInput, 0f);
     }
     private void Move()
     {
@@ -53,7 +53,7 @@ public class PlayerMovement : MonoBehaviour
             speedVar = moveSpeed;
         }
         var currentPos = new Vector3(PlayerT.position.x, PlayerT.position.y, PlayerT.position.z);
-        PlayerT.position = currentPos + new Vector3(hInput, vInput, 0) * speedVar * Time.deltaTime;
+        PlayerT.position = currentPos + moveVector * speedVar * Time.deltaTime;
     }
     //change this so that if walking one direction and then change direction the current face direction doesn't change
     private void Emote()
@@ -61,22 +61,28 @@ public class PlayerMovement : MonoBehaviour
         //get the current animation clip
         walk_dir = anim.GetCurrentAnimatorClipInfo(0)[0].clip.name;
         //horizontal input, ignore if up or down
-        if (hInput > 0)
+        if (moveVector != Vector3.up && moveVector != Vector3.down)
         {
-            walk_dir = "walk_right";
-        }
-        else if (hInput < 0)
-        {
-            walk_dir = "walk_left";
+            if (hInput > 0)
+            {
+                walk_dir = "walk_right";
+            }
+            else if (hInput < 0)
+            {
+                walk_dir = "walk_left";
+            }
         }
         //vertical movement
-        if (vInput > 0)
+        else if (moveVector != Vector3.right && moveVector != Vector3.left)
         {
-            walk_dir = "walk_up";
-        }
-        else if (vInput < 0)
-        {
-            walk_dir = "walk_down";
+            if (vInput > 0)
+            {
+                walk_dir = "walk_up";
+            }
+            else if (vInput < 0)
+            {
+                walk_dir = "walk_down";
+            }
         }
         //if nothing changed play the last animation clip
         anim.Play(walk_dir);
