d11dd9138c7ff72cf562a0757a2b8dc5764cf12b
BenB <boyer.benlee@gmail.com>
2022-07-11 16:02:36 -0700

    edited key message box

--- Assets/Scripts/Interactables/PickUpable.cs
+++ Assets/Scripts/Interactables/PickUpable.cs
@@ -8,14 +8,14 @@ using UnityEngine;
 public class PickUpable : MonoBehaviour
 {
     //public GameObject that renders the text for picking up object, we could getComponentInChildren or use a tag, but this is easier right now, bc I don't know if we'll have other textrenderers
-    public GameObject textRenderer;
+    public GameObject textBox;
     [System.NonSerialized] public bool pickedUp;
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
             //make a new coroutine that displays the message
-            textRenderer.SetActive(true);
+            textBox.SetActive(true);
 
             //masterKey.GetComponent<KeyBuilder>().addKey(gameObject);
             //GetComponent<Collider2D>().enabled = false;
@@ -27,19 +27,19 @@ public class PickUpable : MonoBehaviour
         if (other.gameObject.CompareTag("Player"))
         {
             //make a new coroutine that displays the message
-            textRenderer.SetActive(false);
+            textBox.SetActive(false);
         }
 
     }
     private void Start()
     {
-        textRenderer.SetActive(false);
+        textBox.SetActive(false);
     }
     private void Update()
     {
         ProcessInputs();
         //since we set the prop active onTrigger, we use that boolean
-        if (pickedUp && textRenderer.activeSelf == true) 
+        if (pickedUp && textBox.activeSelf == true) 
         {
             onPickUp();
         }
