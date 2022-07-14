608e0f763b451043c6c87820a0f9fb252e9e583f
BenB <boyer.benlee@gmail.com>
2022-07-14 11:40:44 -0700

    added some player item filler

--- Assets/Scripts/Models/PlayerItem.cs
+++ Assets/Scripts/Models/PlayerItem.cs
@@ -4,9 +4,19 @@ public abstract record PlayerItem
     public abstract string Description { get; protected set; }
     public int Amount;
 }
-//subclasses
+//direct subclasses
 public record Gold : PlayerItem
 {
     public override string Name { get; protected set; } = "Gold"!;
     public override string Description { get; protected set; } = "The standard currency"!;
+}
+public record Weapon : PlayerItem 
+{
+    public override string Name { get; protected set; }
+    public override string Description { get; protected set; }
+}
+public record Disc : Weapon
+{
+    public override string Name { get; protected set; } = "Disc"!;
+    public override string Description { get; protected set; } = "A deadly throwing disc wiht razor sharp edges"!;
 }
