023ac46b16445dee02ffede0b848488a717a72e0
BenB <boyer.benlee@gmail.com>
2022-09-17 19:19:01 -0700

    altered coroutines with some mock logic

--- Assets/Scripts/Managers/BattleManager.cs
+++ Assets/Scripts/Managers/BattleManager.cs
@@ -15,6 +15,8 @@ public class BattleManager : MonoBehaviour
     private GameObject player;
     private Transform playerT;
     private Transform enemyT;
+    private float playerSpeed = 1f;
+    private float enemySpeed = 1f;
     private void Start()
     {
         //instantiate gameObjects in battle scene
@@ -24,26 +26,38 @@ public class BattleManager : MonoBehaviour
         playerT = player.transform;
         enemyT.position = enemyStart.position;
         playerT.position =playerStart.position;
+        StartCoroutine("playerActions");
     }
     private void FixedUpdate()
     {
-        if (playerT.position.x < playerWaypoint.position.x) 
+        if (playerT.position.x < playerWaypoint.position.x)
         {
-            StartCoroutine("movePlayer");
+            playerT.position = playerT.position + new Vector3(playerSpeed, 0f, 0f) * Time.deltaTime;
         }
+/*        if (playerT.position.x == playerStart.position.x) 
+        {
+            StartCoroutine("playerActions");
+        }*/
         if (enemyT.position.x > enemyWaypoint.position.x)
         {
             StartCoroutine("moveEnemy");
         }
     }
-    private IEnumerator movePlayer() 
+    private IEnumerator playerActions() 
     {
-        playerT.position = playerT.position + new Vector3(.1f, 0f, 0f);
-        yield return new WaitForSeconds(1);
+        //we should probably put menu access/prompts here and obviously name it something other than movePlayer
+        //each step of the battle should be here as part of the enumerator
+        //we can change speed here as well
+        yield return new WaitForSeconds(1.5f);
+        Debug.Log("speed increased to player");
+        playerSpeed += 1f;
+        yield return new WaitForSeconds(1f);
+        Debug.Log("speed decreased to player");
+        playerSpeed -= 1.2f;
     }
     private IEnumerator moveEnemy()   
     {
-        enemyT.position = enemyT.position - new Vector3(.1f, 0f, 0f);
-        yield return new WaitForSeconds(1);
+        enemyT.position = enemyT.position - new Vector3(enemySpeed, 0f, 0f) * Time.deltaTime;
+        yield return null;
     }
 }
